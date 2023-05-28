a3141a3f0b49463aa02ce7639e4f3b718df92f6a
lapanikeugene <eugeny.lp@gmail.com>
2023-05-28 19:44:09 +0200

    env added.

--- src/components/GalleryItems/service/getPictures.ts
+++ src/components/GalleryItems/service/getPictures.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import { graphqlQuery } from "./graphqlQuery";
 
 export const getPictures = async()=>{
-
-    const res = await axios.get('',
+    console.log('show url:',import.meta.env.VITE_URL)
+    const res = await axios.get(import.meta.env.VITE_URL,
     {
         params:{
             query:graphqlQuery
